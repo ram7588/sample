e1bc2ef75d6199600042b7b994b77b6c7e072191
ompatil <om2438164@gmail.com>
2024-05-08 08:52:10 +0530

    commited

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const hostname = '127.0.0.1';
+const port = 3000;
+app.use(express.static('public'));
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
